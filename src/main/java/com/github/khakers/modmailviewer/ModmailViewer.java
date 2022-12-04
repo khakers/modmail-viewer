3338ebbda97c5da852708bbfab8c1ee1a3e775a9
Khakers <22665282+khakers@users.noreply.github.com>
2022-12-03 22:05:34 -0800

    Fix version numbering and add log on startup

--- src/main/java/com/github/khakers/modmailviewer/ModmailViewer.java
+++ src/main/java/com/github/khakers/modmailviewer/ModmailViewer.java
@@ -12,7 +12,12 @@ public class ModmailViewer {
 
     public static final String PRECISE_GIT_VERSION_ID;
 
+    public static final String VERSION;
+
+    public static final String BRANCH;
+
     static {
+        String PRECISE_GIT_VERSION_ID_TEMP;
         var gitProperties = new Properties();
         try (var gitPropertiesStream = ModmailViewer.class.getResourceAsStream("/git.properties")) {
             gitProperties.load(gitPropertiesStream);
@@ -21,9 +26,18 @@ public class ModmailViewer {
         }
 
         BUILD_TIMESTAMP = Instant.parse(gitProperties.getProperty("buildTimestamp"));
-
+        BRANCH = gitProperties.getProperty("git.branch", "NULL");
         COMMIT_ID = gitProperties.getProperty("git.commit.id.abbrev", "");
-        PRECISE_GIT_VERSION_ID = gitProperties.getProperty("git.commit.id.describe", "UNKNOWN VERSION ID");
+        PRECISE_GIT_VERSION_ID_TEMP = gitProperties.getProperty("git.commit.id.describe", "UNKNOWN VERSION ID");
+        if (PRECISE_GIT_VERSION_ID_TEMP.isBlank()) {
+            PRECISE_GIT_VERSION_ID_TEMP = "UNKNOWN VERSION ID";
+        }
+        PRECISE_GIT_VERSION_ID = PRECISE_GIT_VERSION_ID_TEMP;
+        if (BRANCH.equalsIgnoreCase("HEAD") && gitProperties.getProperty("git.dirty", "false").equals("false")) {
+            VERSION = gitProperties.getProperty("git.build.version", "");
+        } else {
+            VERSION = gitProperties.getProperty("git.build.version", "")+"-"+BRANCH+"-DIRTY";
+        }
 
     }
 }
