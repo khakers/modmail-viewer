469ade818dd5b1e43457bf0a33fdc344e84422c9
Khakers <22665282+khakers@users.noreply.github.com>
2023-02-19 19:10:16 -0800

    Fix theme selection

--- src/main/resources/static/js/ThemeSelector.js
+++ src/main/resources/static/js/ThemeSelector.js
@@ -4,73 +4,52 @@
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
  */
 
-// up.compiler('#themeSelector', function (element) {
-//     showActiveTheme(getPreferredTheme())
-//
-//     document.querySelectorAll('[data-bs-theme-value]')
-//         .forEach(toggle => {
-//             toggle.addEventListener('click', () => {
-//                 const theme = toggle.getAttribute('data-bs-theme-value')
-//                 localStorage.setItem('theme', theme)
-//                 setTheme(theme)
-//                 showActiveTheme(theme)
-//             })
-//         })
-// })
+'use strict'
 
-(() => {
-    'use strict'
+const storedTheme = localStorage.getItem('theme')
 
-    const storedTheme = localStorage.getItem('theme')
-
-    const getPreferredTheme = () => {
-        if (storedTheme) {
-            return storedTheme
-        }
-
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+const getPreferredTheme = () => {
+    if (storedTheme) {
+        return storedTheme
     }
 
-    const setTheme = function (theme) {
-        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.documentElement.setAttribute('data-bs-theme', 'dark')
-        } else {
-            document.documentElement.setAttribute('data-bs-theme', theme)
-        }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = function (theme) {
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', theme)
     }
+}
 
-    setTheme(getPreferredTheme())
+setTheme(getPreferredTheme())
 
-    const showActiveTheme = theme => {
-        const activeThemeIcon = document.querySelector('.theme-icon-active use')
-        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+const showActiveTheme = theme => {
+    const activeThemeIcon = document.querySelector('.theme-icon-active use')
+    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
 
-        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-            element.classList.remove('active')
-        })
-
-        btnToActive.classList.add('active')
-        activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-    }
-
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-        if (storedTheme !== 'light' || storedTheme !== 'dark') {
-            setTheme(getPreferredTheme())
-        }
+    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+        element.classList.remove('active')
     })
 
-    window.addEventListener('DOMContentLoaded', () => {
-        showActiveTheme(getPreferredTheme())
+    btnToActive.classList.add('active')
+    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+}
 
-        document.querySelectorAll('[data-bs-theme-value]')
-            .forEach(toggle => {
-                toggle.addEventListener('click', () => {
-                    const theme = toggle.getAttribute('data-bs-theme-value')
-                    localStorage.setItem('theme', theme)
-                    setTheme(theme)
-                    showActiveTheme(theme)
-                })
-            })
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    if (storedTheme !== 'light' || storedTheme !== 'dark') {
+        setTheme(getPreferredTheme())
+    }
+})
+
+up.compiler('[data-bs-theme-value]', function (element) {
+    element.addEventListener('click', () => {
+        const theme = element.getAttribute('data-bs-theme-value')
+        localStorage.setItem('theme', theme)
+        setTheme(theme)
+        showActiveTheme(theme)
     })
-})()
+})
