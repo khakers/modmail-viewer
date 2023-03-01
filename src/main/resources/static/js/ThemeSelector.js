ba26f57d12deefad1769d12b2b1fad0601719fa1
Khakers <22665282+khakers@users.noreply.github.com>
2023-02-28 23:11:19 -0800

    feat: sync hljs style to selected color scheme

--- src/main/resources/static/js/ThemeSelector.js
+++ src/main/resources/static/js/ThemeSelector.js
@@ -1,6 +1,6 @@
 /*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2022 The Bootstrap Authors
+ * Modified from color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2022 The Bootstrap Authors, Khakers
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
  */
 
@@ -19,8 +19,27 @@ const getPreferredTheme = () => {
 const setTheme = function (theme) {
     if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
+        document.querySelector("link[title=dark]").removeAttribute("disabled");
+        document.querySelector("link[title=light]").setAttribute("disabled", "disabled");
+    } else if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: light)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'light')
+        document.querySelector("link[title=light]").removeAttribute("disabled");
+        document.querySelector("link[title=dark]").setAttribute("disabled", "disabled");
     } else {
         document.documentElement.setAttribute('data-bs-theme', theme)
+        switch (theme) {
+            case "dark": {
+                document.querySelector("link[title=dark]").removeAttribute("disabled");
+                document.querySelector("link[title=light]").setAttribute("disabled", "disabled");
+                break;
+            }
+            case "light": {
+                document.querySelector("link[title=light]").removeAttribute("disabled");
+                document.querySelector("link[title=dark]").setAttribute("disabled", "disabled");
+                break;
+            }
+        }
+
     }
 }
 
@@ -53,3 +72,4 @@ up.compiler('[data-bs-theme-value]', function (element) {
         showActiveTheme(theme)
     })
 })
+
