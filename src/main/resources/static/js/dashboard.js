1a000df797cec1745bf5c28b7f5d66d981b55e18
Khakers <22665282+khakers@users.noreply.github.com>
2023-06-03 17:53:20 -0700

    feat(frontend): migrate chart instantiation code to unpoly compilers

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -36,166 +36,184 @@ observer.observe(document.documentElement, {
     attributes: true
 })
 
+up.compiler('#ticketClosersChart', (element) => {
+    const ticketClosersCanvas = element;
 
-const ticketClosersCanvas = document.getElementById('ticketClosersChart');
+    let ticketClosersChartData = JSON.parse(element.dataset.chartData);
+    // console.log(ticketClosersChartData)
 
-let ticketClosersChartData = JSON.parse(ticketClosersCanvas.dataset.chartData);
-console.log(ticketClosersChartData)
-
-const ticketClosersChart = new Chart(ticketClosersCanvas.getContext('2d'), {
-    type: 'doughnut',
-    data: {
-        //https://github.com/kurkle/chartjs-plugin-autocolors/issues/20
-        // The autocolor plugin is dumb and if the data doesn't exist it won't ever get a color when it updates
-        labels: ticketClosersChartData.labels,
-        datasets: [{
-            label: 'Tickets closed',
-            data: ticketClosersChartData.data,
-            hoverOffset: 8,
-            offset: 4,
-            borderWidth: 0,
-            borderJoinStyle: 'bevel',
-            // borderColor: 'rgba(0,0,0,0)'
-        }]
-    },
-
-    options: {
-        normalized: true,
-        layout: {
-            padding: 15
+    const ticketClosersChart = new Chart(ticketClosersCanvas.getContext('2d'), {
+        type: 'doughnut',
+        data: {
+            //https://github.com/kurkle/chartjs-plugin-autocolors/issues/20
+            // The autocolor plugin is dumb and if the data doesn't exist it won't ever get a color when it updates
+            labels: ticketClosersChartData.labels,
+            datasets: [{
+                label: 'Tickets closed',
+                data: ticketClosersChartData.data,
+                hoverOffset: 8,
+                offset: 4,
+                borderWidth: 0,
+                borderJoinStyle: 'bevel',
+                // borderColor: 'rgba(0,0,0,0)'
+            }]
         },
-        plugins: {
-            autocolors: {
-                enabled: true,
-                mode: 'data'
-            }
-        }
-    }
-});
-charts.push(ticketClosersChart);
 
-const ticketActionsPerDayCanvas = document.getElementById('ticketActionsPerDayChart');
-
-let ticketActionsPerDayData = JSON.parse(ticketActionsPerDayCanvas.dataset.chartData);
-console.log(ticketActionsPerDayData)
-
-
-const ticketActionsPerDayChart = new Chart(ticketActionsPerDayCanvas.getContext('2d'), {
-    type: 'line',
-    response: true,
-    data: {
-        labels: ticketActionsPerDayData.labels,
-        datasets: [
-            {
-                label: 'Tickets Closed',
-                data: ticketActionsPerDayData.data[1],
-                // hoverOffset: 8,
-                // borderWidth: 1
-                // backgroundColor: 'rgb(210,41,41)',
-                // borderColor: 'rgb(210,41,41)',
-                backgroundColor: style.getPropertyValue('--bs-red'),
-                borderColor: style.getPropertyValue('--bs-red'),
-
-                tension: 0.3,
-                borderWidth: 4,
-                borderJoinStyle: 'round',
-                pointStyle: false
+        options: {
+            normalized: true,
+            layout: {
+                padding: 15
             },
-            {
-                label: 'New Tickets',
-                data: ticketActionsPerDayData.data[0],
-                // backgroundColor: 'rgb(16,129,29)',
-                // borderColor: 'rgb(16,129,29)',
-                backgroundColor: style.getPropertyValue('--bs-green'),
-                borderColor: style.getPropertyValue('--bs-green'),
-
-                tension: 0.3,
-                borderWidth: 4,
-                pointStyle: false
+            plugins: {
+                autocolors: {
+                    enabled: true,
+                    mode: 'data'
+                }
             }
-        ]
-    },
-    scales: {
-        y: {
-            min: 0,
-            ticks: {
-                stepsize: 1
-            }
-        },
-        default: {
-            axis: 'y',
-            min: 0
         }
-    },
+    });
+    charts.push(ticketClosersChart);
+    // console.log(charts)
 
-    options: {
-        interaction: {
-            intersect: false,
-            mode: 'index',
+    up.destructor(element, () => {
+        //remove the chart from the charts array by comparing the canvas to element
+        charts.splice(charts.findIndex((chart) => chart.canvas === element), 1);
+
+        ticketClosersChart.destroy();
+    });
+});
+
+up.compiler('#ticketActionsPerDayChart', (element) => {
+    const ticketActionsPerDayCanvas = element;
+
+    let ticketActionsPerDayData = JSON.parse(ticketActionsPerDayCanvas.dataset.chartData);
+    // console.log(ticketActionsPerDayData)
+
+
+    const ticketActionsPerDayChart = new Chart(ticketActionsPerDayCanvas.getContext('2d'), {
+        type: 'line',
+        response: true,
+        data: {
+            labels: ticketActionsPerDayData.labels,
+            datasets: [
+                {
+                    label: 'Tickets Closed',
+                    data: ticketActionsPerDayData.data[1],
+                    // hoverOffset: 8,
+                    // borderWidth: 1
+                    // backgroundColor: 'rgb(210,41,41)',
+                    // borderColor: 'rgb(210,41,41)',
+                    backgroundColor: style.getPropertyValue('--bs-red'),
+                    borderColor: style.getPropertyValue('--bs-red'),
+
+                    tension: 0.3,
+                    borderWidth: 4,
+                    borderJoinStyle: 'round',
+                    pointStyle: false
+                },
+                {
+                    label: 'New Tickets',
+                    data: ticketActionsPerDayData.data[0],
+                    // backgroundColor: 'rgb(16,129,29)',
+                    // borderColor: 'rgb(16,129,29)',
+                    backgroundColor: style.getPropertyValue('--bs-green'),
+                    borderColor: style.getPropertyValue('--bs-green'),
+
+                    tension: 0.3,
+                    borderWidth: 4,
+                    pointStyle: false
+                }
+            ]
         },
-        plugins: {
-            autocolors: {
-                enabled: true
+        scales: {
+            y: {
+                min: 0,
+                ticks: {
+                    stepsize: 1
+                }
+            },
+            default: {
+                axis: 'y',
+                min: 0
             }
         },
-        normalized: true,
-        layout: {
-            padding: 15
-        },
-    }
+
+        options: {
+            interaction: {
+                intersect: false,
+                mode: 'index',
+            },
+            plugins: {
+                autocolors: {
+                    enabled: true
+                }
+            },
+            normalized: true,
+            layout: {
+                padding: 15
+            },
+        }
+    });
+    charts.push(ticketActionsPerDayChart)
+
+    up.destructor(element, () => {
+        //remove the chart from the charts array by comparing the canvas to element
+        charts.splice(charts.findIndex((chart) => chart.canvas === element), 1);
+
+        ticketActionsPerDayChart.destroy();
+    });
 });
-charts.push(ticketActionsPerDayChart)
 
 up.on('click', "#dashboardReloadButton", (event, element) => {
     up.reload();
 });
 
-function refreshTicketClosersChart() {
-    fetch('/api/ticketclosers')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            // data.ord
-            ticketClosersChart.data.datasets[0].data = [];
-            ticketClosersChart.data.labels = [];
-
-            for (const key in data) {
-                ticketClosersChart.data.datasets[0].data.push(data[key]);
-                ticketClosersChart.data.labels.push(key);
-            }
-            // ticketClosersChart.reset()
-            // ticketClosersChart._autocolor = undefined
-            ticketClosersChart.update();
-            // console.log(labels)
-            // console.log(values)
-        });
-}
-
-document.getElementById("chartPeriodSelect").addEventListener("change", ev => {
-    refreshTicketActionsPerDayChart();
-});
-
-function refreshTicketActionsPerDayChart() {
-    const selector = document.getElementById("dashboardPeriodSelect");
-    // selector.value
-
-    fetch('/api/stats/ticketactivity?' + new URLSearchParams({
-        period: selector.value, status: 'CLOSED'
-    }))
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            ticketActionsPerDayChart.data.labels = data.labels;
-            ticketActionsPerDayChart.data.datasets[0].data = data.data;
-            ticketActionsPerDayChart.update();
-        })
-    fetch('/api/stats/ticketactivity?' + new URLSearchParams({
-        period: selector.value, status: 'OPEN'
-    }))
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            ticketActionsPerDayChart.data.datasets[1].data = data.data;
-            ticketActionsPerDayChart.update();
-        })
-}
+// function refreshTicketClosersChart() {
+//     fetch('/api/ticketclosers')
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data)
+//             // data.ord
+//             ticketClosersChart.data.datasets[0].data = [];
+//             ticketClosersChart.data.labels = [];
+//
+//             for (const key in data) {
+//                 ticketClosersChart.data.datasets[0].data.push(data[key]);
+//                 ticketClosersChart.data.labels.push(key);
+//             }
+//             // ticketClosersChart.reset()
+//             // ticketClosersChart._autocolor = undefined
+//             ticketClosersChart.update();
+//             // console.log(labels)
+//             // console.log(values)
+//         });
+// }
+//
+// document.getElementById("chartPeriodSelect").addEventListener("change", ev => {
+//     refreshTicketActionsPerDayChart();
+// });
+//
+// function refreshTicketActionsPerDayChart() {
+//     const selector = document.getElementById("dashboardPeriodSelect");
+//     // selector.value
+//
+//     fetch('/api/stats/ticketactivity?' + new URLSearchParams({
+//         period: selector.value, status: 'CLOSED'
+//     }))
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data);
+//             ticketActionsPerDayChart.data.labels = data.labels;
+//             ticketActionsPerDayChart.data.datasets[0].data = data.data;
+//             ticketActionsPerDayChart.update();
+//         })
+//     fetch('/api/stats/ticketactivity?' + new URLSearchParams({
+//         period: selector.value, status: 'OPEN'
+//     }))
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data);
+//             ticketActionsPerDayChart.data.datasets[1].data = data.data;
+//             ticketActionsPerDayChart.update();
+//         })
+// }
