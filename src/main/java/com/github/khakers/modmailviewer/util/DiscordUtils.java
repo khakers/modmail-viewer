fa7dcb3d34c4578be0b7db6da3d0b25abcd69b4b
Khakers <22665282+khakers@users.noreply.github.com>
2023-06-04 22:03:06 -0700

    feat: Add better support for discord username migration
    Currently, discriminators will be hidden if the user has migrated to a new username. We can't show display names since modmail doesn't support them and probably won't for the foreseeable future

---
+++ src/main/java/com/github/khakers/modmailviewer/util/DiscordUtils.java
@@ -0,0 +1,38 @@
+package com.github.khakers.modmailviewer.util;
+
+import com.github.khakers.modmailviewer.data.User;
+
+public class DiscordUtils {
+    /**
+     * Returns true if the user is a migrated user (i.e. has no discriminator)
+     *
+     * @param user The user to check
+     * @return True if the user is a migrated user
+     */
+    public static boolean isMigratedUserName(User user) {
+        return user.discriminator().equals("0") || user.discriminator().isBlank();
+    }
+
+    /**
+     * Returns the avatar ID for the user
+     *
+     * @param user The user to get the avatar ID for
+     * @return The avatar ID for the user
+     */
+    public static int getAvatarId(User user) {
+        if (isMigratedUserName(user))
+            return (int) ((Long.parseLong(user.id()) >> 22) % 5);
+        else
+            return Integer.parseInt(user.discriminator()) % 5;
+    }
+
+    /**
+     * Returns the avatar URL for the user
+     *
+     * @param user The user to get the avatar URL for
+     * @return The avatar URL for the user
+     */
+    public static String getAvatarUrl(User user) {
+        return "https://cdn.discordapp.com/embed/avatars/" + getAvatarId(user) + ".png";
+    }
+}
