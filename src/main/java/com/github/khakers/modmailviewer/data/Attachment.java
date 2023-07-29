70ab2bba0dd04e688d7a2e7417954f4eebd645e0
Khakers <22665282+khakers@users.noreply.github.com>
2023-07-29 13:42:26 -0700

    feat: add contentType support to Attachment

--- src/main/java/com/github/khakers/modmailviewer/data/Attachment.java
+++ src/main/java/com/github/khakers/modmailviewer/data/Attachment.java
@@ -3,14 +3,21 @@ package com.github.khakers.modmailviewer.data;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.BsonType;
 import org.bson.codecs.pojo.annotations.BsonRepresentation;
+import org.jetbrains.annotations.Nullable;
 
 public record Attachment(
-        @BsonRepresentation(BsonType.STRING)
-        long id,
-        String filename,
-        String url,
-        @JsonProperty("is_image")
-        boolean isImage,
-        int size
+      @BsonRepresentation(BsonType.STRING)
+      long id,
+      String filename,
+      String url,
+      //isImage does not correctly Identify whether this is an image
+      @JsonProperty("is_image")
+      boolean isImage,
+      int size,
+
+      //Requires modmail enhanced feature support
+      @JsonProperty("content_type")
+      @Nullable
+      String contentType
 ) {
 }
