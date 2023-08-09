0f2be31cbd3d39d2ca08967d25530c3caadd1b5f
Khakers <22665282+khakers@users.noreply.github.com>
2023-08-08 22:27:43 -0700

    fix: add JsonProperty annotation for UserToken.discordRoles property

--- src/main/java/com/github/khakers/modmailviewer/auth/UserToken.java
+++ src/main/java/com/github/khakers/modmailviewer/auth/UserToken.java
@@ -2,6 +2,7 @@ package com.github.khakers.modmailviewer.auth;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Arrays;
 import java.util.Objects;
@@ -14,6 +15,7 @@ public class UserToken {
     String discriminator;
     String avatar;
 
+    @JsonProperty("roles")
     long[] discordRoles = new long[]{};
 
     @JsonIgnore
