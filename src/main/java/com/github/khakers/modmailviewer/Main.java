khakers/modmail-viewer

commit 485fbda310517381f551ac1fd937384718e54025

feat: add System Properties config source and fix use of deprecated addSource methods

src/main/java/com/github/khakers/modmailviewer/Main.java
@@ -53,8 +53,9 @@ import org.github.gestalt.config.Gestalt;
 import org.github.gestalt.config.builder.GestaltBuilder;
 import org.github.gestalt.config.exceptions.GestaltException;
 import org.github.gestalt.config.path.mapper.SnakeCasePathMapper;
-import org.github.gestalt.config.source.ClassPathConfigSource;
-import org.github.gestalt.config.source.EnvironmentConfigSource;
+import org.github.gestalt.config.source.ClassPathConfigSourceBuilder;
+import org.github.gestalt.config.source.EnvironmentConfigSourceBuilder;
+import org.github.gestalt.config.source.SystemPropertiesConfigSourceBuilder;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
@@ -104,8 +105,9 @@ public class Main {
         Gestalt gestalt = new GestaltBuilder()
               .setTreatNullValuesInClassAsErrors(true)
               .setTreatMissingValuesAsErrors(false)
-              .addSource(new ClassPathConfigSource("/default.properties"))
-              .addSource(new EnvironmentConfigSource(envPrepend))
+              .addSource(ClassPathConfigSourceBuilder.builder().setResource("/default.properties").build())
+              .addSource(EnvironmentConfigSourceBuilder.builder().setPrefix(envPrepend).setFailOnErrors(true).build())
+              .addSource(SystemPropertiesConfigSourceBuilder.builder().setFailOnErrors(true).build())
               .addDefaultPathMappers()
               .addPathMapper(new SnakeCasePathMapper())
 //              .addSource(new ClassPathConfigSource("/default.properties"))  // Load the default property files from resources.

commit 78258c29b7ef9252553beba27308838ffeb16e99

fix: change setRemovePrefix to true on EnvironmentConfigLoader
This fixes the regression introduced in 485fbda3 that broke loading of environment variables

src/main/java/com/github/khakers/modmailviewer/Main.java
@@ -110,10 +110,10 @@ public class Main {
                     .build()) // Load the default property files from resources.
               .addSource(EnvironmentConfigSourceBuilder.builder()
                     .setPrefix(envPrepend)
-                    .setFailOnErrors(true)
+                    .setRemovePrefix(true)
                     .build())
               .addSource(SystemPropertiesConfigSourceBuilder.builder()
-                    .setFailOnErrors(true)
+                    .setFailOnErrors(false)
                     .build())
               .addDefaultPathMappers()
               .addPathMapper(new SnakeCasePathMapper())