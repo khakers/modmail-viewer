8f132684ad264aa48670db0f2e420acd186f0ff1
Khakers <22665282+khakers@users.noreply.github.com>
2024-04-27 22:34:55 -0700

    feat(core): Add support fo unknown properties to ModmailConfig

--- src/main/java/com/github/khakers/modmailviewer/data/modmail/ModmailConfig.java
+++ src/main/java/com/github/khakers/modmailviewer/data/modmail/ModmailConfig.java
@@ -1,8 +1,6 @@
 package com.github.khakers.modmailviewer.data.modmail;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import com.github.khakers.modmailviewer.auth.Role;
 
 import java.util.HashMap;
@@ -12,21 +10,18 @@ import java.util.Objects;
 
 @JsonIgnoreProperties({"_id"})
 public class ModmailConfig {
-
+    @JsonIgnore
+    Map<String, Object> unknownFields = new HashMap<>();
     @JsonProperty("bot_id")
     private long botID;
-
     @JsonProperty("close_emoji")
     private String closeEmoji;
-
     @JsonProperty("level_permissions")
     private Map<Role, List<Long>> levelPermissions;
     @JsonProperty("log_channel_id")
     private long logChannelId;
-
     @JsonProperty("main_category_id")
     private long mainCategoryId;
-
     @JsonProperty("recipient_thread_close")
     private int recipientThreadClose;
     @JsonIgnore
@@ -56,6 +51,17 @@ public class ModmailConfig {
         return recipientThreadClose;
     }
 
+    @JsonAnyGetter
+    public Map<String, Object> getUnknownFields() {
+        return unknownFields;
+    }
+
+    @JsonAnySetter
+    public void add(String key, Object value) {
+        unknownFields.put(key, value);
+    }
+
+
     @JsonIgnore
     public Map<Long, Role> getFlatUserPerms() {
         if (flatUserPermMap != null) {
@@ -72,16 +78,25 @@ public class ModmailConfig {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ModmailConfig that = (ModmailConfig) o;
-        return botID == that.botID && logChannelId == that.logChannelId && mainCategoryId == that.mainCategoryId && recipientThreadClose == that.recipientThreadClose && closeEmoji.equals(that.closeEmoji) && levelPermissions.equals(that.levelPermissions);
+        return botID == that.botID && logChannelId == that.logChannelId && mainCategoryId == that.mainCategoryId && recipientThreadClose == that.recipientThreadClose && Objects.equals(unknownFields, that.unknownFields) && Objects.equals(closeEmoji, that.closeEmoji) && Objects.equals(levelPermissions, that.levelPermissions) && Objects.equals(flatUserPermMap, that.flatUserPermMap);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(botID, closeEmoji, levelPermissions, logChannelId, mainCategoryId, recipientThreadClose);
+        return Objects.hash(unknownFields, botID, closeEmoji, levelPermissions, logChannelId, mainCategoryId, recipientThreadClose, flatUserPermMap);
     }
 
     @Override
     public String toString() {
-        return "ModmailConfig{" + "botID=" + botID + ", closeEmoji='" + closeEmoji + '\'' + ", levelPermissions=" + levelPermissions + ", logChannelId=" + logChannelId + ", mainCategoryId=" + mainCategoryId + ", recipientThreadClose=" + recipientThreadClose + '}';
+        return "ModmailConfig{" +
+              "unknownFields=" + unknownFields +
+              ", botID=" + botID +
+              ", closeEmoji='" + closeEmoji + '\'' +
+              ", levelPermissions=" + levelPermissions +
+              ", logChannelId=" + logChannelId +
+              ", mainCategoryId=" + mainCategoryId +
+              ", recipientThreadClose=" + recipientThreadClose +
+              ", flatUserPermMap=" + flatUserPermMap +
+              '}';
     }
 }
